9eba86e56bdf8b84aae5bc8859c282bddd89857f
Roman Korobchuk <Kromanyonets@gmail.com>
2019-01-16 15:06:41 +0200

    Add files via upload

---
+++ actions/test_data.py
@@ -0,0 +1,16 @@
+# Here we store the necessary tests URL's.
+home_page = 'http://automationpractice.com/index.php'
+product_page = 'http://automationpractice.com/index.php?id_product=3&controller=product'
+
+# Personal info
+email = 'automation_test@test.com'
+name = 'Ivan'
+surname ='Pelykh'
+password = 'Password1!'
+
+# Address info
+address = 'test street'
+city = 'Lviv'
+zip = 708000
+mobile_phone = '0636665544'
+alias = 'Alias@alias.com'
